2c22f44939dfa1ca305e77a36835d562b8220f52
Gutsulyak Dmitry <doberq@gmail.com>
2015-07-27 16:08:27 +0300

    Move schemes with required geo-settings to shared folder and remove unused code

--- Therewego Watch App/Therewego Watch App Extension/Application/ExtensionDelegate.m
+++ Therewego Watch App/Therewego Watch App Extension/Application/ExtensionDelegate.m
@@ -14,12 +14,6 @@
     // Session objects are always available on Apple Watch thus there is no use in calling +WCSession.isSupported method.
     [WCSession defaultSession].delegate = self;
     [[WCSession defaultSession] activateSession];
-    
-    // Set up a location manager.
-    CLLocationManager *locationManager = [CLLocationManager new];
-    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
-        [locationManager requestAlwaysAuthorization];
-    }
 }
 
 - (void)applicationDidBecomeActive {
